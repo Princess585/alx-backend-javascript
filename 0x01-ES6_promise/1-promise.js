04499dcd2bfb4b8f63dc2d54b3038248ce5a1299
Princess585 <princessdkhombisa1994@gmail.com>
2024-08-07 11:08:02 +0000

    updated

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,12 @@
-function getFullResponseFromAPI(success) {
-  return new promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' })
-    } else {
-      reject(new Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(arg) {
+ return new Promise((resolve, reject) => {
+    if (arg === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else if (arg === false) {
+      reject(Error('The fake API is not working currently'));
     }
-  })
+  });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,21 +1,14 @@
-export function uploadPhoto() {
-  return Promise.resolve({
-    status: 200,
-    body: 'photo-profile-1',
-  });
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  return Promise.all([promise1, promise2])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
-
-
-export function createUser() {
-  return Promise.resolve({
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-  });
-}
-
-function handleProfileSignup() {
-  const promise = [uploadPhoto(), createUser()]
-  console.log(`${resolve[0].body}
-${resolve[1].firstName} ${resolve.[1].lastName}`
-  })
-}.
